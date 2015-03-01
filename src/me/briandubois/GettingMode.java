4071c4c61ee5779ad4ec4232040d384dec14329c
Brian <bdub2885@gmail.com>
2015-03-01 17:24:03 -0500

    Mode finger hw create

---
+++ src/me/briandubois/GettingMode.java
@@ -0,0 +1,59 @@
+package me.briandubois;
+
+import java.util.Random;
+
+/**
+ * Created by Brian on 3/1/2015.
+ */
+public class GettingMode {
+
+    public static int mode1(int[] a)
+    {
+        int mode = 0;
+        int modeCount = 0;
+
+        for (int i = 0; i <= 100; i++)
+        {
+            int currentCount = 0;
+
+            for (int j = 0; j < a.length; j++)
+            {
+                if (a[j] == i)
+                    currentCount++;
+            }
+
+            if (currentCount > modeCount)
+                mode = i;
+        }
+        return mode;
+    }
+
+    public static int mode2(int[] a)
+    {
+        int[] nums = new int[100];
+        int HIndex = -1;
+        int HIndexNum = -1;
+
+        for (int i = 0; i < a.length; i++)
+        {
+            nums[a[i]]++;
+            if (nums[a[i]] > HIndexNum)
+                HIndex = a[i];
+        }
+        return HIndex;
+
+    }
+
+    public static void main(String[] args) {
+        int[] usedissun = new int[100];
+        Random r = new Random();
+
+        for (int i = 0; i < usedissun.length; i++)
+        {
+            usedissun[i] = r.nextInt(101);
+        }
+
+        mode1(usedissun);
+        mode2(usedissun);
+    }
+}
